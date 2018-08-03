f98acfdd13617606cbb43c452387602948049c71
wanghan <xiaoyaoyangeren@163.com>
2018-08-03 20:24:59 +0800

    commit lession one and two

---
+++ lession2/mathsuite.py
@@ -0,0 +1,22 @@
+# coding=utf-8
+
+import HTMLTestRunner
+import unittest
+from lession2.test_unit_case import *
+
+def gen_report():
+    suite = unittest.TestSuite()
+
+    suite.addTests(unittest.TestLoader().loadTestsFromTestCase(TestMathFunc))
+
+    filename = "test_math_report.html"
+    with open(filename, "w") as f:
+        runner = HTMLTestRunner.HTMLTestRunner(
+            stream=f,
+            title="test math func report",
+            description="case result"
+        )
+        runner.run(suite)
+
+if __name__ == '__main__':
+    gen_report()
---
+++ lession2/test_unit_case.py
@@ -0,0 +1,45 @@
+import unittest
+from unittest import skip
+from lession2.mathfunc import *
+
+class TestMathFunc(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        print("所有用例执行前所执行的")
+
+    def setUp(self):
+        print("每个用例执行前所执行的")
+
+    @skip("跳过不执行")
+    def test_add(self):
+        # 以test开头
+        # self.assertEqual(add(1, 1), 3, "not equal")
+        # self.assertEqual(add(10, 29), 39)
+        # self.assertGreaterEqual(add(1,2), 3)
+        self.assertLessEqual(add(2, 2), 6, "not less equal")
+
+    @unittest.skip("临时跳过不执行")
+    def test_minus(self):
+        self.assertEqual(minus(10, 3), 7)
+        self.assertEqual(minus(3, 9), -6)
+
+    def test_multi(self):
+        self.assertEqual(multi(3, 8), 24)
+        self.assertEqual(multi(6, 7), 42)
+
+    def test_divide(self):
+        self.assertEqual(divide(8, 2), 4)
+        self.assertEqual(divide(9, 3), 3)
+
+    def tearDown(self):
+        print("每个用例执行完后所执行的")
+
+    @classmethod
+    def tearDownClass(cls):
+        print("所有用例执行完后所执行的")
+
+
+if __name__ == '__main__':
+    # verbosity指执行结果输出详细程度，0=简单， 1=一般， 2=详细
+    unittest.main(verbosity=2)
